b4eac02ec0608c901c4109876b259bcb688796e5
Julian Schrittwieser <j.schrittwieser@gmail.com>
2021-09-05 14:33:06 +0100

    prettier alert dialog

--- app/src/main/java/org/furidamu/wakari_chan/DictOverlayActivity.kt
+++ app/src/main/java/org/furidamu/wakari_chan/DictOverlayActivity.kt
@@ -7,6 +7,11 @@ import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import org.w3c.dom.Text
 import java.io.File
 
 val TAG = "WakariChan";
@@ -22,20 +27,17 @@ class DictOverlayActivity : Activity() {
             val db = openDb();
             val translation = translate(selectedText, db);
 
+
             // Use the Builder class for convenient dialog construction
             val builder = AlertDialog.Builder(this)
-            builder.setMessage(translation)
-                .setNeutralButton("Cancel",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        finish();
-                    })
+            builder.setView(translation)
                 .setOnDismissListener(DialogInterface.OnDismissListener { dialog -> finish(); })
             builder.show()
 
         }
     }
 
-    fun translate(text: String, dict: SQLiteDatabase): String {
+    fun translate(text: String, dict: SQLiteDatabase): View {
         val c = dict.rawQuery(
             "SELECT reading, kind, english FROM translations where word = ? ORDER BY priority DESC LIMIT 100",
             arrayOf(text)
@@ -55,21 +57,49 @@ class DictOverlayActivity : Activity() {
             kind.putIfAbsent(reading, c.getString(kindCol));
         }
 
-        var translation = "";
+
+        val alertView = LinearLayout(this);
+        alertView.setOrientation(LinearLayout.VERTICAL);
+        alertView.setPadding(20, 10, 10, 20);
+
+        var i = 0;
         for ((reading, english) in translations) {
-            translation += "$text ($reading)\n";
-            translation += "(${kind.get(reading)})";
+            val wordView = TextView(this);
+            wordView.text = text;
+            wordView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20.0f);
+            wordView.setTextColor(0xff9fc5e8.toInt()); // light blue
+
+            val readingView = TextView(this);
+            readingView.text = "  $reading";
+            readingView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20.0f);
+            readingView.setTextColor(0xffb6d7a8.toInt()); // light green
+
+            val japanese = LinearLayout(this);
+            japanese.addView(wordView);
+            japanese.addView(readingView);
+            japanese.setPadding(0,  if (i == 0)  0 else 20 , 0, 6);
+
+            alertView.addView(japanese);
+
+            var text = "(${kind.get(reading)})";
             if (english.size > 1) {
                 english.forEachIndexed { i, e ->
-                    translation += " ($i) $e";
+                   text += " ($i) $e";
                 }
             } else {
-                translation += " ${english[0]}";
+                text += " ${english[0]}";
             }
-            translation += "\n";
+
+            val translationView = TextView(this);
+            translationView.text = text;
+            translationView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16.0f);
+
+            alertView.addView(translationView);
+
+            ++i;
         }
 
-        return translation;
+        return alertView;
     }
 
     fun openDb(): SQLiteDatabase {
